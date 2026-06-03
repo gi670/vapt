03e183d579c9b4b24d444d060d0529158599df56
Jerma swarn J <jermaswarn12@gmail.com>
2026-06-03 22:18:36 +0530

    Add files via upload

---
+++ dbconnection/connection.php
@@ -0,0 +1,61 @@
+<?php
+	// $place = $_REQUEST['place'];
+	// session_start();
+	// $_SESSION['place'] = $place;
+	if($place == '1')
+	{
+		include 'dbconnection/kan_connection.php';
+	}
+	else if($place == '2')
+	{
+		include 'dbconnection/chen_connection.php';
+	}
+	else if($place == '3')
+	{
+		include 'dbconnection/chen_connection.php';
+	}
+	else if($place == '4')
+	{
+		include 'dbconnection/chen_connection.php';
+	}
+	else if($place == '5')
+	{
+		include 'dbconnection/chen_connection.php';
+	}
+	else if($place == '6')
+	{
+		include 'dbconnection/chen_connection.php';
+	}
+	else if($place == '7')
+	{
+		include 'dbconnection/chen_connection.php';
+	}
+	else if($place == '8')
+	{
+		include 'dbconnection/chen_connection.php';
+	}
+	else if($place == '9')
+	{
+		include 'dbconnection/chen_connection.php';
+	}
+	else if($place == '10')
+	{
+		include 'dbconnection/chen_connection.php';
+	}	
+	else if($place == '17')
+	{
+		include 'dbconnection/chen_connection.php';
+	}
+	else if($place == '19')
+	{
+		include 'dbconnection/thodupuzha_connection.php';
+	}
+	else if($place == '22')
+	{
+		include 'dbconnection/haripad_connection.php';
+	}
+	else if($place == '24')
+	{
+		include 'dbconnection/kan1_connection.php';
+	}
+?>
